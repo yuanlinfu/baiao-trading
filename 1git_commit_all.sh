c36a2a255b174e1f9fec4eb0bcbfd9899245b94a
yuanlinfu <348476126@qq.com>
2026-04-06 08:13:40 +0800

    修复问题，更新代码

---
+++ 1git_commit_all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# =========================================================
+# 脚本名称: 1git_commit_all.sh
+# 功能: git全部提交
+# =========================================================
+
+set -e
+# 提交信息设置
+COMMIT_MSG="修复问题，更新代码"
+############################
+# 基础变量设置
+############################
+
+# 项目根目录设置
+#BASE_DIR="/var/www/${PROJECT_NAME}"
+BASE_DIR="$(pwd)"
+
+
+#COMMIT_MSG="dockercompose文件更新"
+
+echo "提交前仓库状态"
+git status
+
+# 提交仓库
+echo "git add ."
+git add .
+echo "git commit -m ${COMMIT_MSG}"
+git commit -m "${COMMIT_MSG}"
+
+echo "之后推送到远程仓库只需要运行:"
+echo "git push"
+
+echo "当前仓库状态"
+git status
+
+# 快速执行
+# sudo bash 1git_commit_all.sh && sudo git push
